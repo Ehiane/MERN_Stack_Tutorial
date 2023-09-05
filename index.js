67e30c8bbc406913dbb95ef7c2e5173470c6f61e
Ehiane <ehis.oigiagbe@gmail.com>
2023-09-05 14:03:01 -0700

    changes, and added route

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { error } from "console";
+import authRoutes from "./routes/auth.js"
 import {register} from "./controllers/auth.js"
 
 //#USE 'nodemon run' to run server
@@ -45,8 +45,8 @@ const upload = multer({storage})
 /*middle ware Funct*/ 
 app.post("/auth/register", upload.single("picture"), register) //all the uploaded pictures will be locally uploaded to 'public/assests'
 
-
-
+/*ROUTES*/ 
+app.use("/auth", authRoutes);
 
 /*MONGOOSE SETUP*/
 
