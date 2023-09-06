006b0ebc9967dd3f5b6daf314669adecc1c760bf
Ehiane <ehis.oigiagbe@gmail.com>
2023-09-05 20:17:27 -0700

    editing stuff done

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,4 +43,24 @@ export const register = async(req, res) => {
         // when smth goes wrong
         res.status(500).json({error: err.message })
     }
+};
+
+
+/*LOGGING IN*/ 
+export const login = async(req, res) =>{
+    try{
+        const {email, password} = req.body; //grabbing email and pswrd when user tries to login
+        const user = await User.findOne({email: email}); //using mongoose to find the one with this specified email
+        if(!user) return res.status(400).json({msg: "User does not exist."})
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({msg: "Invalid credentials."})
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({token, user});
+        
+    }catch(err){
+        res.status(500).json({error: err.message })
+    }
 }
